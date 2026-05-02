78e82ee2ba61f1d31981e337b9c95ba85a35ff45
loonettevillegas <164777713+loonettevillegas@users.noreply.github.com>
2026-05-02 14:01:13 -0400

    Teams logic
    Logica de equipos

--- multiplayer_example/scripts/statics.gd
+++ multiplayer_example/scripts/statics.gd
@@ -13,7 +13,34 @@ enum Role {
 	ROLE_C,
 	ROLE_D
 }
+enum Team {
+	NONE,
+	TEAM_BLACK,
+	TEAM_WHITE
+}
 
+
+
+static func get_team_name(team: Team) -> String:
+	match team:
+		Team.NONE:
+			return "None"
+		Team.TEAM_BLACK:
+			return "Black"
+		Team.TEAM_WHITE:
+			return "White"
+	return "Unknown"
+	
+	
+static func get_team_from_role(role: Role) -> Team:
+	match role:
+		Role.ROLE_A, Role.ROLE_B:
+			return Team.TEAM_BLACK
+		Role.ROLE_C, Role.ROLE_D:
+			return Team.TEAM_WHITE
+		_:
+			return Team.NONE
+			
 
 static func get_role_name(role: Role) -> String:
 	match role:
@@ -36,6 +63,8 @@ class PlayerData:
 	# Position relative to other players
 	var index: int = -1
 	var role: Role
+	var team: Team
+
 	var vote: bool = false
 	
 	func _init(new_id: int, new_name: String, new_index: int = -1, new_role: Role = Role.NONE) -> void:
@@ -43,6 +72,9 @@ class PlayerData:
 		name = new_name
 		index = new_index
 		role = new_role
+		team = Statics.get_team_from_role(new_role)
+
+		
 	
 	func _to_string() -> String:
 		return "Player: {id: %d, name: %s, index: %d, role: %d}" % [id, name, index, Statics.get_role_name(role)]
@@ -53,12 +85,15 @@ class PlayerData:
 			"name": name,
 			"index": index,
 			"role": role,
+			"team":team,
 			"vote": vote
 		}
 	
 	static func from_dict(data: Dictionary) -> PlayerData:
 		var player = PlayerData.new(data.id, data.name, data.index, data.role)
 		player.vote = data.vote
+		player.team = data.team 
+
 		return player
 	
 	func update(player_data: PlayerData) -> void:
@@ -67,4 +102,6 @@ class PlayerData:
 		name = player_data.name
 		index = player_data.index
 		role = player_data.role
+		team = player_data.team
+
 		vote = player_data.vote
