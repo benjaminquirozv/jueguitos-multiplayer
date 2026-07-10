68f7cbfdd838239eac9c0ec7bc78ec07d5f9e408
Benjamin Quiroz <benjamin.quiroz.v@ug.uchile.cl>
2026-07-09 23:26:33 -0400

    colision invisible working

---
+++ multiplayer_example/level1/barrera_2.gd
@@ -0,0 +1,9 @@
+extends TileMapLayer
+
+func _ready() -> void:
+	# Oculta la capa visualmente, pero mantiene las colisiones activas
+	visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
